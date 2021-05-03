39ca9269eab9fc40259f3f14850454d9dd00971f
AndreAguirreRoman <ankar_aguirre@live.com>
2021-05-03 17:00:43 -0500

    Created post component

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,20 @@
+import { fetchRecentPosts } from '../actions';
+import {
+    SET_RECENT_POSTS
+} from '../actions/types';
+
+const INIT_STATE = {
+    posts: [],
+    recentPosts: []
+}
+
+export default function (state = INIT_STATE, action) {
+    switch (action.type) {
+        case SET_RECENT_POSTS:
+            const recentPosts = action.payload
+            return { ...state, recentPosts };
+
+        default:
+            return state;
+    }
+}
